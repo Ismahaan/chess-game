029720211bb8086b0f1b70d06076c4989bf7238a
Ismahaan Mohamed <IsmahaanMohamed@outlook.com>
2019-09-26 21:39:01 +0200

    error

--- ChessPiece.py
+++ ChessPiece.py
@@ -1,5 +1,21 @@
 from abc import ABCMeta, abstractmethod
+import Board
 class ChessPiece(metaclass=ABCMeta):
     position = 0
+    
     @abstractmethod
+    
+    def _init_(self,x,y,color):
+        self.position_x = x
+        self.position_y = y
+        self.color = color
+   
+class Rook(ChessPiece):
+    def _init_(self,x,y,color,board):
+        ChessPiece._init_(self,x,y,color)
+        self.symbol = "R"
+        board[self.position_x][self.position_y] = self
+
+
+    
 
