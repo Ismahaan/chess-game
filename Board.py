f714b20db3c4e964640c2f821145edf1ae338288
Ismahaan Mohamed <IsmahaanMohamed@outlook.com>
2019-09-27 21:25:57 +0200

    'crwtw'

--- Board.py
+++ Board.py
@@ -1,8 +1,39 @@
 import numpy as np
+from ChessPiece import Queen
+from ChessPiece import Knight
 from ChessPiece import Rook
+from ChessPiece import King
+from ChessPiece import Bishop
+from ChessPiece import Pawn
+
 
 class Board:
-    board = np.zeros([8,8], dtype = object)
+    board = np.full([8,8],'0' ,dtype = object)
     Rook1 = Rook(0,0,'w',board)
-    # Undefined variable 'Rook'pylint(undefined-variable)
+    Rook2 = Rook(0,7,'w',board)
+    Knight1 = Knight(0,1,'w',board)
+    Knight2 = Knight(0,6,'w',board)
+    Bishop1 = Bishop(0,2,'w',board)
+    Bishop2 = Bishop(0,5,'w',board)
+    Kingz = King(7,4,'z',board)
+    Queenz = Queen(7,3,'z',board)
+    King = King(0,3,'w',board)
+    Queen = Queen(0,4,'w',board)
+   
+   
+    Pawnn=[]
+    for x in range(8):
+        Pawnn.append(Pawn(1,x,'w',board))
+
+    Rookz1 = Rook(7,0,'z',board)
+    Rookz2 = Rook(7,7,'z',board)
+    Knightz1 = Knight(7,1,'z',board)
+    Knightz2 = Knight(7,6,'z',board)
+    Bishopz1 = Bishop(7,2,'z',board)
+    Bishopz2 = Bishop(7,5,'z',board)
+    
+
+    Pawnz=[]
+    for x in range(8):
+        Pawnz.append(Pawn(6,x,'z',board))
     print(board)
--- ChessPiece.py
+++ ChessPiece.py
@@ -14,3 +14,35 @@ class Rook(ChessPiece):
         ChessPiece.__init__(self,x,y,color)
         self.symbol = "R"
         board[self.position_x][self.position_y] = self.symbol
+
+class King(ChessPiece):
+    def __init__(self,x,y,color,board):
+        ChessPiece.__init__(self,x,y,color)
+        self.symbol = "K"
+        # self.count = 0
+        board[self.position_x][self.position_y] = self.symbol
+
+class Bishop(ChessPiece):
+    def __init__(self,x,y,color,board):
+        ChessPiece.__init__(self,x,y,color)
+        self.symbol = "B"
+        board[self.position_x][self.position_y] = self.symbol
+
+class Knight(ChessPiece):
+    def __init__(self,x,y,color,board):
+        ChessPiece.__init__(self,x,y,color)
+        self.symbol = "k"
+        board[self.position_x][self.position_y] = self.symbol
+
+class Pawn(ChessPiece):
+    def __init__(self,x,y,color,board):
+        ChessPiece.__init__(self,x,y,color)
+        self.symbol = "P"
+        self.count = 0
+        board[self.position_x][self.position_y] = self.symbol
+
+class Queen(ChessPiece):
+    def __init__(self,x,y,color,board):
+        ChessPiece.__init__(self,x,y,color)
+        self.symbol = "Q"
+        board[self.position_x][self.position_y] = self.symbol
