9aa0311e031c250cacf750827eb5e2f9654a2e0b
Daan van Schie <0967090@hr.nl>
2019-09-27 11:18:53 +0200

    import error fixed

--- Board.py
+++ Board.py
@@ -1,9 +1,8 @@
 import numpy as np
-from abc import ABCMeta, abstractmethod
+from ChessPiece import Rook
+
 class Board:
-    board = np.zeros([8,8], dtype = int)
+    board = np.zeros([8,8], dtype = object)
     Rook1 = Rook(0,0,'w',board)
     # Undefined variable 'Rook'pylint(undefined-variable)
     print(board)
-    
-        
--- ChessPiece.py
+++ ChessPiece.py
@@ -1,21 +1,16 @@
 from abc import ABCMeta, abstractmethod
-import Board
+
 class ChessPiece(metaclass=ABCMeta):
-    position = 0
     
     @abstractmethod
     
-    def _init_(self,x,y,color):
+    def __init__(self,x,y,color):
         self.position_x = x
         self.position_y = y
         self.color = color
    
 class Rook(ChessPiece):
-    def _init_(self,x,y,color,board):
-        ChessPiece._init_(self,x,y,color)
+    def __init__(self,x,y,color,board):
+        ChessPiece.__init__(self,x,y,color)
         self.symbol = "R"
-        board[self.position_x][self.position_y] = self
-
-
-    
-
+        board[self.position_x][self.position_y] = self.symbol
